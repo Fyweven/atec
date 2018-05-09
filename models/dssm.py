b668cba89caf11b97244005e4426d64af923c819
fuyuwen <fuyuwen@wusongtech.com>
2018-05-10 00:12:34 +0800

    Modify the config

--- models/dssm.py
+++ models/dssm.py
@@ -8,5 +8,5 @@ class DSSM(nn.Module):
     def __init__(self):
         super(DSSM, self).__init__()
         self.model_name = 'DSSM'
-        self.encoder = nn.Embedding(opt.char_size, opt.embedding_dim)
-        
+        # self.encoder = nn.Embedding(opt.char_size, opt.embedding_dim)
+        self.ques_encoder = nn.Linear(opt.char_size, )
--- utils/config.py
+++ utils/config.py
@@ -7,6 +7,8 @@ class Config(object):
     batch_size = 64
     char_size = 1718
     embedding_dim = 128
+    L1_N = 400
+    L2_N = 120
 
     orig_data_path = '/data/atec_nlp_sim_train.csv'
     char_idx_path = '/data/pkl/char2idx.pkl'
